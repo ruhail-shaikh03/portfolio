b194b8279d5dd372093d69af1f7f375c0ad11094
Ruhail Rizwan <rohail.shaikh0301@gmail.com>
2025-01-17 02:19:11 +0500

    jjj

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "rohail.shaikh0301@gmail.com"; // Your email address
+    $subject = "New Contact Form Submission from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+}
+?>
