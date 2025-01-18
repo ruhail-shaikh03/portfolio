b39c12c588ba174619adc9b0d5f74e92a9c17d97
Ruhail Rizwan <rohail.shaikh0301@gmail.com>
2025-01-18 05:12:51 +0500

    gg

---
+++ script.js
@@ -0,0 +1,5 @@
+function openPortfolio(portfolioId) {
+    // Open a new page with portfolio details
+    const portfolioPage = `portfolio${portfolioId}.html`;
+    window.location.href = portfolioPage;
+}
